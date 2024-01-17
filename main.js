26553fba0ab20ba80c5afb33decd54bbba464f28
Riyagrrm <120349265+Riyagrrm@users.noreply.github.com>
2024-01-17 11:39:12 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1 = ""
 song2 = ""
+left_x = ""
+left_y = ""
+right_x = ""
+right_y = ""
 function preload()
 {
     song1 = loadSound("music.mp3")
@@ -11,8 +15,25 @@ function setup()
     canvas.center()
     video = createCapture(VIDEO)
     video.hide()
+    posenet = ml5.poseNet(video, modelLoaded)
+    posenet.on("pose", gotPoses)
+}
+function modelLoaded()
+{
+    console.log("modelLoaded")
 }
 function draw()
 {
     image(video, 0, 0, 600, 500)
+}
+function gotPoses(results)
+{
+    if (results.length > 0)
+    {
+        console.log(results)
+        left_x = results[0].pose.leftWrist.x
+        left_y = results[0].pose.leftWrist.y
+        right_x = results[0].pose.rightWrist.x
+        right_y = results[0].pose.rightWrist.y
+    }
 }
